f2fe5ebb18c7fbbeaf3d3983e294bd6cc33bc3c1
Jai Indra <jaiindra2026@u.northwestern.edu>
2026-02-26 02:41:41 -0600

    path update #3

--- app/components/ExhibitionModules.tsx
+++ app/components/ExhibitionModules.tsx
@@ -1,23 +1,29 @@
+import React from "react";
 import { exhibitions } from "../js/exhibitions";
+import { getBasePath } from "../js/getBasePath";
 
 export default function ExhibitionModules() {
+  const base = getBasePath();
+
   return (
     <div className="modules-container">
       {exhibitions.map((exhibit) => (
         <a
           key={exhibit.slug}
-          href={`../exhibitions/${exhibit.slug}`}
+          href={`${base}/exhibitions/${exhibit.slug}/`}
           className="module-box"
         >
+          {/* TEXT SIDE */}
           <div className="text">
-            <h3>{exhibit.title}</h3>
+            <h2>{exhibit.title}</h2>
             <p>{exhibit.description}</p>
           </div>
 
+          {/* IMAGE SIDE */}
           <div className="image">
             <img
-              src={exhibit.image}
-              alt={`${exhibit.title} preview`}
+              src={`${base}${exhibit.image}`}
+              alt={exhibit.title}
             />
           </div>
         </a>
---
+++ app/js/getBasePath.js
@@ -0,0 +1,9 @@
+export function getBasePath() {
+  if (typeof window !== "undefined") {
+    if (window.location.pathname.startsWith("/EIS-Final")) {
+      return "/EIS-Final";
+    }
+  }
+
+  return "";
+}
