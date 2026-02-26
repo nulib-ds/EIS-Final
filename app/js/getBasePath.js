e704c546d780a22de63ece0c9f4f8f1aca861ef0
Jai Indra <jaiindra2026@u.northwestern.edu>
2026-02-26 02:46:35 -0600

    Comment explanation of basepath

--- app/js/getBasePath.js
+++ app/js/getBasePath.js
@@ -1,9 +1,9 @@
+// Ensures
 export function getBasePath() {
   if (typeof window !== "undefined") {
     if (window.location.pathname.startsWith("/EIS-Final")) {
       return "/EIS-Final";
     }
   }
-
   return "";
 }
