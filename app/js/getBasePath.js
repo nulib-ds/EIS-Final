8e3044a00f137bc82a038b9a76a1f39cf89f04d7
Jai Indra <jaiindra2026@u.northwestern.edu>
2026-02-26 02:47:02 -0600

    Basepath explanation

--- app/js/getBasePath.js
+++ app/js/getBasePath.js
@@ -1,9 +1,11 @@
-// Ensures
+// Ensures correct base path for images and links on GitHub Pages and local development //
+
 export function getBasePath() {
   if (typeof window !== "undefined") {
     if (window.location.pathname.startsWith("/EIS-Final")) {
       return "/EIS-Final";
     }
   }
+
   return "";
 }
