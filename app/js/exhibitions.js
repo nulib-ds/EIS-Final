ff121c2729c4489261483d6bf1903b808f1dbe7c
Jai Indra <jaiindra2026@u.northwestern.edu>
2026-02-26 02:05:02 -0600

    Resolve Image routing on Github w absolute paths

--- app/js/exhibitions.js
+++ app/js/exhibitions.js
@@ -4,23 +4,20 @@ export const exhibitions = [
     title: "Priced Out",
     description:
       "This exhibition focuses on housing security, gentrification, and community displacement in the U.S.",
-    image:
-      "../images/home/chicago-bw.webp",
+    image: "/images/home/chicago-bw.webp"
   },
   {
     slug: "the-daily-grind",
     title: "The Daily Grind",
     description:
       "This exhibition explores the origins of the infrastructure debates—mass transit vs. cars, and pedestrian safety vs. traffic flow that dominate city planning.",
-    image:
-      "../images/home/chicago-bw.webp",
+    image: "/images/home/chicago-bw.webp"
   },
   {
     slug: "invisible-hazards",
     title: "Invisible Hazards",
     description:
       'This exhibition features documents on the health hazards of lead-based paint, specifically addressing "pica" in children.',
-    image:
-      "../images/home/chicago-bw.webp",
+    image: "/images/home/chicago-bw.webp"
   },
 ];
